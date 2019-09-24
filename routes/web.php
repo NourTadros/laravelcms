919c8cde8faa173d1df507518c571682e9b22599
NourTadros <36901160+NourTadros@users.noreply.github.com>
2019-09-24 16:12:08 +0200

    .

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,36 @@ Route::get('/contact','PostsController@contact');
 Route::get('post/{id}/{name}/{password}','PostsController@show_post');
 
 
+//INSERT RAW SQL QUERIES
+// Route::get('/insert',function(){
+//     DB::insert('insert into posts(title,content)[ values(?,?)',['PHP with laravel','PHP Laravel is the best thing with stomach ache']);
+// });
+
+
+// Route::get('/read',function(){
+// $results=DB::select('select * from posts where id=? ',[1]);
+
+// foreach($results as $post){
+//     return $post->title;
+// }
+
+// });
+
+// Route::get('/update',function(){
+// $updated=DB::update('update posts set title = "PHP with laravel framework" where id=?',[1]);
+
+// return $updated;
+// });
+
+// Route::get('/delete',function(){
+// $deleted=DB::delete('delete from posts where id=?',[1]);
+
+// return $deleted;
+// });
+
+
+
+
 
 
 
