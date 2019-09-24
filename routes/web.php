78215df1c309f0eee163389efd5cb13f1d0fc141
NourTadros <36901160+NourTadros@users.noreply.github.com>
2019-09-24 16:37:36 +0200

    eloquent read

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,5 @@
 <?php
+use App\Post;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,13 +21,13 @@ Route::get('/', function () {
 // Route::resource('posts','PostsController');
 
 Route::get('/contact','PostsController@contact');
-Route::get('post/{id}/{name}/{password}','PostsController@show_post');
+// Route::get('post/{id}/{name}/{password}','PostsController@show_post');
 
 
 //INSERT RAW SQL QUERIES
-// Route::get('/insert',function(){
-//     DB::insert('insert into posts(title,content)[ values(?,?)',['PHP with laravel','PHP Laravel is the best thing with stomach ache']);
-// });
+Route::get('/insert',function(){
+    DB::insert('insert into posts(title,content) values(?,?)',['PHP with laravel','PHP Laravel is the best thing with stomach ache']);
+});
 
 
 // Route::get('/read',function(){
@@ -52,6 +53,30 @@ Route::get('post/{id}/{name}/{password}','PostsController@show_post');
 
 
 
+//Eloquent
+
+// Route::get('/read',function(){
+//     $posts=Post::all();
+
+//     foreach($posts as $post){
+//         return $post->title;
+
+//     }
+// });
+
+// Route::get('/find',function(){
+//     $post=Post::find(2);
+
+//     return $post->title;
+
+    
+// });
+
+
+Route::get('/search',function(){
+    $posts=Post::where('id',2)->orderBy('id','asc')->take(1)->get();
+    return $posts;
+});
 
 
 
