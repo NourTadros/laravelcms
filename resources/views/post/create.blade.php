575203de4320fe6e4918ca754f7cc01d3101c045
NourTadros <36901160+NourTadros@users.noreply.github.com>
2019-10-10 10:40:54 +0200

    new package

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -5,12 +5,20 @@
 @section('content')
 
 <h1>Create Post</h1>
-<form method="post" action="{{url('posts')}}">
-@csrf
-<input type="text" name="title" placeholder="Enter title">
-<input type="submit"name="submit">
+    {!! Form::open(['method'=>'POST','action'=>'PostsController@store']) !!}
+    <div class="form-group">
 
-</form>
+        {!! Form::label('title', 'Title:') !!}
+        {!! Form::text('title', null, ['class'=>'form-control']) !!}
+
+
+    </div>
+    
+@csrf
+<div class="form-group">
+{!! Form::submit('Create Post', ['class'=>'btn btn-primary']) !!}
+</div>
+{!! Form::close() !!}
 
 
 @endsection
